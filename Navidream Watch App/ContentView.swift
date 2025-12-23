bedd6e8e464d5ba73dac30e5a0b93e01042f56ed
Justin Restivo <justin@restivo.me>
2025-12-23 14:16:20 -0500

    Initial Commit

---
+++ Navidream Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Navidream Watch App
+//
+//  Created by Justin Restivo on 12/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Navidream Watch App/NavidreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavidreamApp.swift
+//  Navidream Watch App
+//
+//  Created by Justin Restivo on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Navidream_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
